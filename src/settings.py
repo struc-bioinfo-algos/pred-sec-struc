54d5c278d49b38b9c7b524cb0e2012698c1cf78d
Michael Krause <michael.krause@jhu.edu>
2021-04-18 14:53:48 -0700

    Settings class namespace for configuration

--- src/settings.py
+++ src/settings.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 
+import configparser
+
+config = configparser.ConfigParser()
+config.read('settings.ini')
+
 
 class Settings:
     """Static class to read settings.ini"""
 
-    @staticmethod
-    def 
+    dssp_path = config.get('DEFAULT', 'DSSP_PATH')
+
