e93654aed9c5331bac21a5bc6472a9405750edf8
Michael Krause <michael.krause@jhu.edu>
2021-04-22 08:21:52 -0700

    WIP: evaluating dssp lines

--- src/settings.py
+++ src/settings.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
 
+import os
 import configparser
 
+PROJECT_ROOT = os.path.dirname(os.getcwd())
+
+
 config = configparser.ConfigParser()
-config.read('settings.ini')
+config.read(os.path.join(PROJECT_ROOT, 'settings.ini'))
 
 
 class Settings:
     """Static class to read settings.ini"""
-
-    dssp_path = config.get('DEFAULT', 'DSSP_PATH')
-
+    config = config
+    dssp_path = config['DEFAULT']['DsspPath']
+    dssp_extension = config['DEFAULT']['DsspExtension']
---
+++ test/test_read_dssp.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import unittest
+from unittest.mock import patch
+from src.settings import Settings
+from src.read_dssp import ReadDSSP
+
+
+class TestReadDSSP(unittest.TestCase):
+
+    def setUp(self) -> None:
+        #self.dssp_path = './test/test.dssp'
+        self.dssp_line2 = '    1  307 A A    >         0   0   56      0, 0.0     3,-0.7     0, 0.0    58,-0.0   0.000 360.0 360.0 360.0 -50.8   15.6  -14.4  -34.4                A         A          1        307          0          0          0          3          0         58\n'
+
+    def test_extract_info_from_line_no_structure(self):
+        aa, dssp_classification, tag = ReadDSSP.extract_info_from_line(self.dssp_line2)
+
+        print(aa, dssp_classification, tag)
+
+    def test_read(self):
+        #MockSettingsDsspPath.return_value = './test'
+        lst = ReadDSSP.read('6vjd')
+        for item in lst:
+            #print(item)
+            pass
+
+    def test_dssp_label_to_category_alpha_helix(self):
+        self.assertEqual(ReadDSSP.dssp_structure_to_category('H'), 'a')
+        self.assertEqual(ReadDSSP.dssp_structure_to_category('I'), 'a')
+        self.assertEqual(ReadDSSP.dssp_structure_to_category('G'), 'a')
+
+    def test_dssp_label_to_category_beta_strand(self):
+        self.assertEqual(ReadDSSP.dssp_structure_to_category('E'), 'b')
+
+    def test_dssp_label_to_category_coil(self):
+        self.assertEqual(ReadDSSP.dssp_structure_to_category('c'), 'c')
+
+
+if __name__ == "__main__":
+    unittest
