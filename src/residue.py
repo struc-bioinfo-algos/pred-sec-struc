9edd558747c941de3b4516691e7bd76ce43ec1c2
Michael Krause <michael.krause@jhu.edu>
2021-04-23 20:14:06 -0700

    Class to hold residue and category info

--- src/residue.py
+++ src/residue.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
 
 import os
+from src.read_dssp import ReadDSSP
 from src.settings import Settings
 
-class EncodeAminoAcids:
+class Residue:
 
-    def __init__(self, pdb_id):
-        self.pdb_id = pdb_id
+    def __init__(self, pdb_id: str):
+        self.pdb_id: str = pdb_id
+        self.read_seq = ReadDSSP
+        self.residue_and_structure: list[tuple]
 
-    def read_seq(self):
+    def set_residue_and_structure(self):
+        residue_and_structure = self.read_seq.read()
         #dssp_path = os.listdir(Settings.dssp_path)
         pass
