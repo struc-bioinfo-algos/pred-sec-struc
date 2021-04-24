541ed5966330df8223f53f7a7bf6a050137e7708
Michael Krause <michael.krause@jhu.edu>
2021-04-23 20:13:24 -0700

    Updated read_dssp methods

--- test/test_read_dssp.py
+++ test/test_read_dssp.py
@@ -1,28 +1,38 @@
 #!/usr/bin/env python3
 
 import unittest
-from unittest.mock import patch
-from src.settings import Settings
+from unittest.mock import patch, PropertyMock
 from src.read_dssp import ReadDSSP
 
 
 class TestReadDSSP(unittest.TestCase):
 
     def setUp(self) -> None:
-        #self.dssp_path = './test/1tes.dssp'
+        self.dssp_test_filename = '1tes'
+        self.dssp_path = './test'
+        self.dssp_line0 = '   77   77 A P  H  > S+     0   0   71      0, 0.0     4,-0.5     0, 0.0    -1,-0.2   0.929 113.9  31.6 -45.4 -53.8  106.3   51.0    1.3                A         A         77         77          0          0          0          4          0         -1\n'
         self.dssp_line2 = '    1  307 A A    >         0   0   56      0, 0.0     3,-0.7     0, 0.0    58,-0.0   0.000 360.0 360.0 360.0 -50.8   15.6  -14.4  -34.4                A         A          1        307          0          0          0          3          0         58\n'
 
+    def test_extract_info_from_line_happy(self):
+        aa, structure, tag = ReadDSSP.extract_info_from_line(self.dssp_line0)
+        self.assertTrue('P' in aa)
+        self.assertTrue('H' in structure)
+        self.assertTrue('OKAY' in tag.name)
+
     def test_extract_info_from_line_no_structure(self):
-        aa, dssp_classification, tag = ReadDSSP.extract_info_from_line(self.dssp_line2)
+        aa, structure, tag = ReadDSSP.extract_info_from_line(self.dssp_line2)
+        self.assertTrue('A' in aa)
+        self.assertTrue(structure.isspace())
+        self.assertTrue('NO_STRUCTURE' in tag.name)
 
-        print(aa, dssp_classification, tag)
-
-    def test_read(self):
-        #MockSettingsDsspPath.return_value = './test'
-        lst = ReadDSSP.read('6vjd')
-        for item in lst:
-            #print(item)
-            pass
+    def test_read_happy(self):
+        with patch('src.settings.Settings.dssp_path', new_callable=PropertyMock) as prop:
+            prop.return_value = self.dssp_path
+            lst = ReadDSSP.read(self.dssp_test_filename)
+            self.assertFalse(lst == [])
+            self.assertTrue(isinstance(lst[0], tuple))
+            self.assertTrue(lst[0].amino_acid == 'P')
+            self.assertTrue(lst[0].category == 'c')
 
     def test_dssp_label_to_category_alpha_helix(self):
         self.assertEqual(ReadDSSP.dssp_structure_to_category('H'), 'a')
