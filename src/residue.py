39a9779318dd71468e9afbe3d1fe646e246bd373
Michael Krause <michael.krause@jhu.edu>
2021-04-24 18:38:42 -0700

    Updated residue

--- src/residue.py
+++ src/residue.py
@@ -1,20 +1,18 @@
 #!/usr/bin/env python3
-
-import os
+import csv
+from sklearn import preprocessing
 from src.read_dssp import ReadDSSP
 from src.settings import Settings
 
 class Residue:
 
-    def __init__(self, pdb_id: str):
+    def __init__(self, pdb_id: str = None):
         self.pdb_id: str = pdb_id
         self.read_seq = ReadDSSP
         self.residue_and_structure: list[tuple]
 
     def set_residue_and_structure(self):
-        residue_and_structure = self.read_seq.read()
-        #dssp_path = os.listdir(Settings.dssp_path)
-        pass
+        self.residue_and_structure = self.read_seq.read(pdb_id=self.pdb_id)
 
 
 class ResidueFactory:
@@ -24,3 +22,29 @@ class ResidueFactory:
 
     def construct(self, instance_names: list) -> dict:
         return {instance_name: Residue(pdb_id=instance_name) for instance_name in instance_names}
+
+
+class AminoAcid:
+    mapping: dict = {}
+
+    @classmethod
+    def get_table(cls) -> dict:
+        with open(Settings.amino_acids, 'r') as csvfile:
+            rows = csv.DictReader(csvfile)
+            for row in rows:
+                cls.mapping[row['Single-letter-abbreviation']] = int(row['Encoding'])
+
+class Target:
+    mapping: dict = {}
+    encoding: preprocessing.LabelEncoder
+
+    @classmethod
+    def get_table(cls) -> dict:
+        # Todo: check whether it is advantageous to use sklearn's LabelEncoder here
+        with open(Settings.target, 'r') as csvfile:
+            rows = csv.DictReader(csvfile)
+            for row in rows:
+                cls.mapping[row['Abbreviation']] = row['Encoding']
+        cls.encoding = preprocessing.LabelEncoder()
+        cls.encoding.fit(list(cls.mapping.keys()))
+
--- test/test_residue.py
+++ test/test_residue.py
@@ -1,11 +1,23 @@
 #!/usr/bin/env python3
 
 import unittest
-from src.residue import Residue, ResidueFactory
+from unittest.mock import patch, PropertyMock
+from src.residue import Residue, ResidueFactory, AminoAcid, Target
 
 
 class TestResidue(unittest.TestCase):
-    pass
+    def setUp(self) -> None:
+        self.dssp_test_filename = '1tes'
+        self.dssp_path = './test'
+
+    def test_set_residue_and_structure(self):
+        with patch('src.settings.Settings.dssp_path', new_callable=PropertyMock) as prop:
+            prop.return_value = self.dssp_path
+            residue = Residue(pdb_id=self.dssp_test_filename)
+            residue.set_residue_and_structure()
+            self.assertEqual(len(residue.residue_and_structure), 509)
+            self.assertEqual(residue.residue_and_structure[384].amino_acid, 'K')
+            self.assertEqual(residue.residue_and_structure[176].category, 'c')
 
 
 class TestResidueFactory(unittest.TestCase):
@@ -28,6 +40,25 @@ class TestResidueFactory(unittest.TestCase):
                 continue
             else:
                 del inst
+
+
+class TestAminoAcid(unittest.TestCase):
+    def test_get_table(self):
+        expected = {
+            'A': 0, 'C': 1, 'D': 2, 'E': 3, 'F': 4, 'G': 5, 'H': 6, 'I': 7, 'K': 8, 'L': 9, 'M': 10,
+            'N': 11, 'P': 12, 'Q': 13, 'R': 14, 'S': 15, 'T': 16, 'V': 17, 'W': 18, 'Y': 19
+        }
+        AminoAcid.get_table()
+        self.assertEqual(expected, AminoAcid.mapping)
+        self.assertEqual(AminoAcid.mapping['S'], 15)
+
+
+class TestTarget(unittest.TestCase):
+    def test_get_table(self):
+        Target.get_table()
+        expected = ['a', 'b', 'c']
+        self.assertEqual(list(Target.encoding.classes_), expected)
+        self.assertEqual(Target.encoding.transform(['c'])[0], 2)
 
 
 if __name__ == '__main__':
