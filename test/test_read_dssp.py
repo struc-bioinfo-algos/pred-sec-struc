7d61a222cda123f0639b9b8b269d281a7dc013bf
Michael Krause <michael.krause@jhu.edu>
2021-04-25 19:38:14 -0700

    Refactored to work in changes in class

--- test/test_read_dssp.py
+++ test/test_read_dssp.py
@@ -12,6 +12,7 @@ class TestReadDSSP(unittest.TestCase):
         self.dssp_path = './test'
         self.dssp_line0 = '   77   77 A P  H  > S+     0   0   71      0, 0.0     4,-0.5     0, 0.0    -1,-0.2   0.929 113.9  31.6 -45.4 -53.8  106.3   51.0    1.3                A         A         77         77          0          0          0          4          0         -1\n'
         self.dssp_line2 = '    1  307 A A    >         0   0   56      0, 0.0     3,-0.7     0, 0.0    58,-0.0   0.000 360.0 360.0 360.0 -50.8   15.6  -14.4  -34.4                A         A          1        307          0          0          0          3          0         58\n'
+        self.dssp_line3 = '   37        !              0   0    0      0, 0.0     0, 0.0     0, 0.0     0, 0.0   0.000 360.0 360.0 360.0 360.0    0.0    0.0    0.0\n'
 
     def test_extract_info_from_line_happy(self):
         aa, structure, tag = ReadDSSP.extract_info_from_line(self.dssp_line0)
@@ -31,8 +32,8 @@ class TestReadDSSP(unittest.TestCase):
             lst = ReadDSSP.read(self.dssp_test_filename)
             self.assertFalse(lst == [])
             self.assertTrue(isinstance(lst[0], tuple))
-            self.assertTrue(lst[0].amino_acid == 'V')
-            self.assertTrue(lst[0].category == 'c')
+            self.assertTrue(lst[0].amino_acid == 'P')
+            self.assertTrue(lst[0].category == 'a')
 
     def test_dssp_label_to_category_alpha_helix(self):
         self.assertEqual(ReadDSSP.dssp_structure_to_category('H'), 'a')
